74c4606ebcbe85eb8288acbc5235b7135309cdd0
miroenlared <miroenlared@gmail.com>
2024-05-23 16:45:07 +0000

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="inner">
+        <h1>Folio 2024</h1>
+        <div class="header-controls">
+            <button>mode</button>
+            <button>lang</button>
+        </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  .inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
